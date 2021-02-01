3ba6944f892d6ff32b3a61783daf5649f91cffac
BradleyL108 <langfbj@pedarecc.sa.edu.au>
2021-02-01 19:12:52 +1030

    Update Colour for windowtesting.py

--- AFL teams/windowtesting.py
+++ AFL teams/windowtesting.py
@@ -4,14 +4,14 @@ window = Tk()
 window.title("Name of window")
 lblwidth = 700
 window.geometry("{}x450".format(lblwidth))
-window.configure(bg="orange")
+window.configure(bg="orange red")
 
 # center this label
-lbl1 = Label(window, text="List", bg="orange", fg="black", font="none 24 bold",
+lbl1 = Label(window, text="List", bg="orange red", fg="black", font="none 24 bold",
              width=lblwidth, anchor=CENTER)
 lbl1.pack()
 
-lbl2 = Label(window, text="Enter something here:", bg="orange",
+lbl2 = Label(window, text="Enter something here:", bg="orange red",
              fg="black", font="none 12 bold", width=lblwidth, anchor=W)
 lbl2.pack()
 
